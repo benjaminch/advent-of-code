df0210010d4356268baa77e46340a54d20ae0333
benjaminch <benjamin.chastanier@gmail.com>
2019-11-30 08:29:34 +0100

    2018/3 read input from stdin

--- 2018/3/solution/src/solution_3.rs
+++ 2018/3/solution/src/solution_3.rs
@@ -2,16 +2,17 @@
 extern crate lazy_static;
 extern crate regex;
 
-use std::io::{self, BufRead};
+use std::io::{self, Read, Write};
 use std::vec::Vec;
-use std::fs::File;
-use std::path::Path;
 use regex::Regex;
 
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
     // Part 1
-    let claims = get_claims(Path::new("../input.txt"));
-    println!("{:?}", claims);
+    let claims = get_claims(&input);
+    writeln!(io::stdout(), "{:?}", claims);
 }
 
 #[derive(Debug)]
@@ -23,31 +24,23 @@ struct Claim {
     height: u32,
 }
 
-fn get_claims(file: &Path) -> Vec<Claim> {
+fn get_claims(input: &str) -> Vec<Claim> {
     let mut claims: Vec<Claim> = Vec::new();
-    if let Ok(lines) = read_lines(file) {
-		for l in lines {
-            if let Ok(line) = l {
-                lazy_static! {
-                    static ref RE: Regex = Regex::new(r"^(?P<id>[\S]+) @ (?P<position_x>[\d]+),(?P<position_y>[\d]+): (?P<width>[\d]+)x(?P<height>[\d]+)").unwrap();
-                }
-                if let Some(caps) = RE.captures(&line) {
-                    let claim = Claim{
-                        id: caps["id"].parse().unwrap(),
-                        position_x: caps["position_x"].parse().unwrap(),
-                        position_y: caps["position_y"].parse().unwrap(),
-                        width: caps["width"].parse().unwrap(),
-                        height: caps["height"].parse().unwrap(),
-                    };
-                    claims.push(claim);
-                };
-            }
+
+    for line in input.lines() {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^(?P<id>[\S]+) @ (?P<position_x>[\d]+),(?P<position_y>[\d]+): (?P<width>[\d]+)x(?P<height>[\d]+)").unwrap();
+        }
+        if let Some(caps) = RE.captures(&line) {
+            let claim = Claim{
+                id: caps["id"].parse().unwrap(),
+                position_x: caps["position_x"].parse().unwrap(),
+                position_y: caps["position_y"].parse().unwrap(),
+                width: caps["width"].parse().unwrap(),
+                height: caps["height"].parse().unwrap(),
+            };
+            claims.push(claim);
         }
     } 
     return claims;
-}
-
-fn read_lines(filename: &Path) -> std::result::Result<std::io::Lines<std::io::BufReader<std::fs::File>>, std::io::Error> {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
