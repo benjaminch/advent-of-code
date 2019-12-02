5a69a2915f8f532132918e24289a4a09a8aa9ca9
benjaminch <benjamin.chastanier@gmail.com>
2019-12-02 07:44:42 +0100

    2019/2/part_1 WIP

---
+++ 2019/2/solution/src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{self, Read, Write};
+use std::vec::Vec;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    
+    // Part 1
+
+    // Part 2
+}
