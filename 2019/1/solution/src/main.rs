b2166a4fcf0dc8c6470ea4f0dcb581e95198b6aa
benjaminch <benjamin.chastanier@gmail.com>
2019-12-03 00:09:46 +0100

    2019/1 refactor

--- 2019/1/solution/src/main.rs
+++ 2019/1/solution/src/main.rs
@@ -1,57 +1,52 @@
 use std::io::{self, Read, Write};
-use std::vec::Vec;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    
+
     // Part 1
-    writeln!(io::stdout(), "Total fuel required: {}", compute_fuel_requirement(&input));
+    writeln!(
+        io::stdout(),
+        "Total fuel required: {}",
+        compute_fuel_requirement(&input, false)
+    )?;
 
     // Part 2
-    writeln!(io::stdout(), "Total fuel required (with fuel weight): {}", compute_fuel_requirement_with_fuel_weight(&input));
+    writeln!(
+        io::stdout(),
+        "Total fuel required (with fuel weight): {}",
+        compute_fuel_requirement(&input, true)
+    )?;
+
+    return Ok(());
 }
 
-fn compute_fuel_requirement(input_modules: &str) -> u32 {
+fn compute_fuel_requirement(input_modules: &str, compute_fuel_for_fuel: bool) -> u32 {
     return input_modules
         .lines()
-        .map(|m| (get_required_fuel_for_module(m.parse::<u32>().unwrap())))
+        .map(|m| (compute_fuel_for_mass(m.parse::<u32>().unwrap(), compute_fuel_for_fuel)))
         .sum();
 }
 
-fn get_required_fuel_for_module(module_weight: u32) -> u32 {
-    let fuel: i32 = (module_weight / 3) as i32 - 2;
-    if fuel <= 0 {
-        return 0;
-    }
-    return fuel as u32;
-}
+fn compute_fuel_for_mass(module: u32, compute_fuel_for_fuel: bool) -> u32 {
+    let mut fuel: u32 = 0;
+    let mut mass: u32 = module;
+    let fuel_for_mass = |weight: u32| -> u32 {
+        let fuel: i32 = (weight / 3) as i32 - 2;
+        if fuel <= 0 {
+            return 0;
+        }
+        return fuel as u32;
+    };
 
-fn compute_fuel_requirement_with_fuel_weight(input_modules: &str) -> u32 {
-    return input_modules
-        .lines()
-        .map(|m| (get_required_fuel_for_module_with_fuel_weight(m.parse::<u32>().unwrap())))
-        .sum();
-}
+    loop {
+        mass = fuel_for_mass(mass);
+        fuel += mass;
 
-fn get_required_fuel_for_module_with_fuel_weight(module_weight: u32) -> u32 {
-    let fuel: i32 = (module_weight / 3) as i32 - 2;
-    if fuel <= 0 {
-        return 0;
-    }
-
-    let mut sum_fuel: u32 = fuel as u32;
-    let mut fuel_for_module = Vec::new();
-    fuel_for_module.push(sum_fuel as i32);
-
-    while let Some(fuel) = fuel_for_module.pop() {
-        let f = (fuel / 3) as i32 - 2;
-        if f > 0 {
-            sum_fuel += f as u32;
-            fuel_for_module.push(f);
-
+        if !compute_fuel_for_fuel || mass <= 0 {
+            break;
         }
     }
 
-    return sum_fuel;
+    return fuel;
 }
