ae941a1012f8799bbc4afda51b6ea1f3c8eeaf43
benjaminch <benjamin.chastanier@gmail.com>
2019-12-04 23:28:51 +0100

    2019/4 fmt

--- 2019/4/solution/src/main.rs
+++ 2019/4/solution/src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{self, Error, Read, Write};
 use std::collections::HashMap;
+use std::io::{self, Error, Read, Write};
 
 fn main() -> Result<(), Error> {
     let mut input = String::new();
@@ -8,21 +8,23 @@ fn main() -> Result<(), Error> {
     let mut lines = input.lines();
     let bounds: Vec<u32> = extract_bounds(lines.next().unwrap().to_string());
 
-    // Part 1 
-    let matching_passwords: u32 =
-        find_matching_passwords(bounds[0], bounds[1], false)
+    // Part 1
+    let matching_passwords: u32 = find_matching_passwords(bounds[0], bounds[1], false)
         .iter()
         .count() as u32;
     writeln!(io::stdout(), "Matching passwords: {}", matching_passwords)?;
 
     // Part 2
-    let matching_passwords2: u32 =
-        find_matching_passwords(bounds[0], bounds[1], true)
+    let matching_passwords2: u32 = find_matching_passwords(bounds[0], bounds[1], true)
         .iter()
         .count() as u32;
-    writeln!(io::stdout(), "Matching passwords (part 2): {}", matching_passwords2)?;
+    writeln!(
+        io::stdout(),
+        "Matching passwords (part 2): {}",
+        matching_passwords2
+    )?;
 
-    return Ok(());
+    Ok(())
 }
 
 fn find_matching_passwords(from: u32, to: u32, exactly_two_adjacent_digits: bool) -> Vec<u32> {
@@ -34,13 +36,11 @@ fn find_matching_passwords(from: u32, to: u32, exactly_two_adjacent_digits: bool
 
         // Initialize making digits from left to right increasing
         for i in 0..from_digits.len() - 1 {
-            if from_digits[i] > from_digits[i+1] {
-                if digit_to_set.is_none() {
-                    digit_to_set = Some(from_digits[i]);
-                }
+            if from_digits[i] > from_digits[i + 1] && digit_to_set.is_none() {
+                digit_to_set = Some(from_digits[i]);
             }
-            if !digit_to_set.is_none() {
-                from_digits[i+1] = digit_to_set.unwrap();
+            if digit_to_set.is_some() {
+                from_digits[i + 1] = digit_to_set.unwrap();
             }
         }
         current = to_number(&from_digits);
@@ -51,42 +51,43 @@ fn find_matching_passwords(from: u32, to: u32, exactly_two_adjacent_digits: bool
         let mut has_two_adjacent_digits: bool = false;
         let mut same = HashMap::<u8, usize>::new();
         for i in 1..from_digits.len() {
-            if from_digits[i-1] == from_digits[i] {
+            if from_digits[i - 1] == from_digits[i] {
                 has_two_adjacent_digits = true;
-                *same.entry(from_digits[i-1]).or_default() += 1;
+                *same.entry(from_digits[i - 1]).or_default() += 1;
             }
         }
 
         if (!exactly_two_adjacent_digits && has_two_adjacent_digits)
-            || (exactly_two_adjacent_digits && same.iter().filter(|x| *x.1 == 1).count() > 0) {
+            || (exactly_two_adjacent_digits && same.iter().filter(|x| *x.1 == 1).count() > 0)
+        {
             passwords.push(current);
         }
 
         current += 1;
     }
-    return passwords;
+    passwords
 }
 
 fn to_number(digits: &Vec<u8>) -> u32 {
-    return digits
+    digits
         .iter()
         .map(|e| e.to_string())
         .collect::<Vec<String>>()
         .join("")
         .parse::<u32>()
-        .unwrap();
+        .unwrap()
 }
 
 fn to_digits(number: u32) -> Vec<u8> {
-    return number
+    number
         .to_string()
         .chars()
         .map(|e| e.to_string().parse::<u8>().unwrap())
-        .collect::<Vec<u8>>();
+        .collect::<Vec<u8>>()
 }
 
 fn extract_bounds(input: String) -> Vec<u32> {
-    let input_split: Vec<&str> = input.split("-").collect::<Vec<&str>>();
+    let input_split: Vec<&str> = input.split('-').collect::<Vec<&str>>();
     let min: u32 = input_split[0].parse::<u32>().unwrap();
     let max: u32 = input_split[1].parse::<u32>().unwrap();
     return vec![min, max];
