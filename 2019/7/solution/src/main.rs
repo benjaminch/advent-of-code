cfe3015dfc12f4df1b17d3f7a4e3589e77f703ff
Benjamin Chastanier <b.chastanier@criteo.com>
2019-12-07 10:51:42 +0100

    2019/7 WIP

---
+++ 2019/7/solution/src/main.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+use std::io::{self, Error, Read, Write};
+
+fn main() -> Result<(), Error> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    // Part 1
+
+    // Part 2
+
+    return Ok(());
+}
