c7ac8dabaa70c518c28850f9237f5f57e108ffb2
Benjamin Chastanier <b.chastanier@criteo.com>
2019-12-15 18:14:18 +0100

    2019/10/part_1

--- 2019/10/solution/src/main.rs
+++ 2019/10/solution/src/main.rs
@@ -1,37 +1,71 @@
-use std::io::{self, Error, ErrorKind,  Read, Write};
+
+use std::collections::HashMap;
+use std::io::{self, Error, ErrorKind, Read, Write};
 
 fn main() -> Result<(), Error> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
+    let map: Map = build_map(input.clone()).unwrap();
+
     // Part 1
-	writeln!(io::stdout(), "{}", build_map(input.clone()).unwrap())?;
+    let best_position: PositionDetected = find_asteroid_detecting_most_asteroids(map).unwrap();
+    writeln!(io::stdout(), "Part 1: {:?}", best_position)?;
 
     // Part 2
 
     return Ok(());
 }
 
-fn get_detected_asteroids(asteroid_position: Position, asteroids_positions: Vec<Position>) -> i32 {
-	let base_vector: (Position, Position) = (Position { x: 0, y: 0 }, Position { x: 0, y: 10 });
-	let mut HashMap<f32, Position> detected_asteroids = HashMap::new();
+fn get_detected_asteroids(
+    asteroid_position: &Position,
+    asteroids_positions: &Vec<Position>,
+) -> usize {
+    let mut detected_asteroids: HashMap<i64, (Position, f32)> = HashMap::new();
 
-	for asteroid in asteroids_positions {
-		// TODO: implement here
-	}
+    for other_asteroid_position in asteroids_positions.iter().filter(|p| p.asteroid.is_some()) {
+        if other_asteroid_position != asteroid_position {
+            let angle: i64 = (((other_asteroid_position.y as i32 - asteroid_position.y as i32)
+                as f64)
+                .atan2(other_asteroid_position.x as f64 - asteroid_position.x as f64)
+                .to_degrees()
+                * 1000000.0)
+                .round() as i64;
+            let other_asteroid_distance: f32 = ((other_asteroid_position.x as i32
+                - asteroid_position.x as i32)
+                .pow(2) as f32
+                + (other_asteroid_position.y as i32 - asteroid_position.y as i32).pow(2) as f32)
+                .sqrt();
+
+            if let Some(detected_asteroid) = detected_asteroids.get(&angle) {
+                if detected_asteroid.1 > other_asteroid_distance {
+                    detected_asteroids
+                        .insert(angle, (*other_asteroid_position, other_asteroid_distance));
+                }
+            } else {
+                detected_asteroids
+                    .insert(angle, (*other_asteroid_position, other_asteroid_distance));
+            }
+        }
+    }
+
+    return detected_asteroids.len();
 }
 
-fn find_asteroid_detecting_more_asteroids(map: Map) -> Option<<Position, i32>> {
-	let mut result: Option<(Position, i32)> = None;
+fn find_asteroid_detecting_most_asteroids(map: Map) -> Option<PositionDetected> {
+    let mut result: Option<PositionDetected> = None;
 
-	for position in map.positions {
-		let detected_asteroids: i32	= get_detected_asteroids(&position);
-		if result.is_none() || detected_asteroids > result.unwrap().1 {
-			result = Some((position, detected_asteroids))
-		} 
-	}
+    for position in map.positions.iter().filter(|p| p.asteroid.is_some()) {
+        let detected_asteroids: usize = get_detected_asteroids(&position, &map.positions);
+        if result.is_none() || detected_asteroids > result.as_ref().unwrap().detected {
+            result = Some(PositionDetected {
+                position: *position,
+                detected: detected_asteroids,
+            });
+        }
+    }
 
-	return result;	
+    return result;
 }
 
 fn build_map(input: String) -> Result<Map, Error> {
@@ -40,36 +74,48 @@ fn build_map(input: String) -> Result<Map, Error> {
     let mut height: usize = 0;
 
     for row in input.lines() {
+        height = 0;
         for cell in row.chars() {
-			positions.push(Position {
-				x: width,
-				y: height,
-				asteroid: match cell {
-					'#' => Some(Asteroid {}),
-					'.' => None,
-					_ => return Err(Error::new(ErrorKind::InvalidInput, "unknown char found!")),
-				}
-			});
+            positions.push(Position {
+                x: height,
+                y: width,
+                asteroid: match cell {
+                    '#' => Some(Asteroid {}),
+                    '.' => None,
+                    _ => return Err(Error::new(ErrorKind::InvalidInput, "unknown char found!")),
+                },
+            });
             height += 1;
         }
         width += 1;
     }
 
+    // println!("{:?}", positions);
+
     return Ok(Map {
-        	width: width,
-        	height: height,
-        	positions: positions,
-    	});
+        width: width,
+        height: height,
+        positions: positions,
+    });
 }
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 struct Asteroid {}
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 struct Position {
     x: usize,
     y: usize,
-	asteroid: Option<Asteroid>,
+    asteroid: Option<Asteroid>,
 }
 
+#[derive(Debug)]
+struct PositionDetected {
+    position: Position,
+    detected: usize,
+}
+
+#[derive(Debug)]
 struct Map {
     width: usize,
     height: usize,
@@ -83,13 +129,12 @@ impl std::fmt::Display for Map {
             if i % (self.width) == 0 {
                 output.push_str("\n");
             }
-            output.push_str(
-				match &self.positions[i].asteroid {
-					Some(_) => "#",
-					None => ".",
-				});
+            output.push_str(match &self.positions[i].asteroid {
+                Some(_) => "#",
+                None => ".",
+            });
         }
-		
+
         write!(f, "{}", output)
     }
 }
